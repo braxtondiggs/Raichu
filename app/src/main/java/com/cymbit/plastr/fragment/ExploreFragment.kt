9e86f5d7db13c94c5aab4dd49aabc06dbe788a55
Braxton Diggs <braxtondiggs@gmail.com>
2019-08-15 01:50:02 -0400

    WIP: Scroll Pagination

--- app/src/main/java/com/cymbit/plastr/fragment/ExploreFragment.kt
+++ app/src/main/java/com/cymbit/plastr/fragment/ExploreFragment.kt
@@ -8,16 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cymbit.plastr.R
 import com.cymbit.plastr.adapter.ExploreAdapter
 import com.cymbit.plastr.helpers.InternetCheck
+import com.cymbit.plastr.helpers.PaginationScrollListener
 import com.cymbit.plastr.service.RedditViewModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_explore.*
 
-
 class ExploreFragment: Fragment() {
     private lateinit var redditViewModel: RedditViewModel
+    private lateinit var after: String
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -34,7 +36,7 @@ class ExploreFragment: Fragment() {
                 if (internet!!) {
                     loadData()
                 } else {
-                    deviceOffline(view).show()
+                    deviceOffline(view).setAction(R.string.try_again) { loadData() }.show()
                 }
             }
         })
@@ -42,22 +44,57 @@ class ExploreFragment: Fragment() {
 
     private fun deviceOffline(view: View): Snackbar {
         return Snackbar.make(view, R.string.offline, Snackbar.LENGTH_INDEFINITE)
-            .setAction(R.string.try_again) { loadData() }
     }
 
     private fun loadData() {
+        var isLastPage = false
+        var isLoading = false
         loadingCircle.visibility = View.VISIBLE
         redditViewModel = ViewModelProviders.of(this).get(RedditViewModel::class.java)
         redditViewModel.fetchData("pics", "")
         redditViewModel.redditLiveData.observe(this, Observer { value ->
-            loadingCircle.visibility = View.GONE
-            swipeLayout.isRefreshing = false
-            rvItems.adapter = ExploreAdapter(value.children)
+            if (value?.before == null) {
+                after = value.after
+                loadingCircle.visibility = View.GONE
+                swipeLayout.isRefreshing = false
+                rvItems.adapter = ExploreAdapter(value.children)
+            } else {
+                // TODO
+            }
             swipeLayout.setOnRefreshListener {
                 redditViewModel.cancelAllRequests()
                 loadData()
             }
+
+            rvItems.addOnScrollListener(object: PaginationScrollListener(rvItems.layoutManager as LinearLayoutManager) {
+                override fun isLastPage(): Boolean {
+                    return isLastPage
+                }
+
+                override fun isLoading(): Boolean {
+                    return isLoading
+                }
+
+                override fun loadMoreItems() {
+                    isLoading = true
+                    loading.visibility = View.VISIBLE
+                    loadMoreData(view!!)
+                }
+            })
         })
 
     }
+
+    private fun loadMoreData(view: View) {
+        InternetCheck(object : InternetCheck.Consumer {
+            override fun accept(internet: Boolean?) {
+                if (internet!!) {
+                    redditViewModel.fetchData("pics", after)
+                } else {
+                    deviceOffline(view).setAction(R.string.try_again) { loadMoreData(view) }.show()
+                }
+            }
+        })
+    }
+
 }
